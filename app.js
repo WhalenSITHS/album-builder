5ee725eb4ba4dfe21587d4919e37a5307bf57de8
mr. whalen <michael.whalen@sitechhs.com>
2020-10-09 13:52:00 -0400

    dfgh

--- app.js
+++ app.js
@@ -1,66 +1,67 @@
-class Album{
-    constructor(title, artist, url) {
-        this.title = title;
-        this.artist = artist;
-        this.url = url;
-    }
+class Album {
+  constructor(title, artist, url) {
+    this.title = title;
+    this.artist = artist;
+    this.url = url;
+  }
 }
 
 class UI {
-    addAlbumToList(album) {
-        const display = document.querySelector('.display');
+  addAlbumToList(album) {
+    let html =
+      '<div class="display-album flex-row"><div class="display-title">%title%</div><div class="display-artist">%artist%</div><div class="display-cover"><img src="%url%"alt=""></div><div class="remove-album">Remove Album X</div>';
+    let newHtml = html.replace("%title%", album.title);
+    newHtml = newHtml.replace("%artist%", album.artist);
+    newHtml = newHtml.replace("%url%", album.url);
+    document.querySelector(".display").insertAdjacentHTML("beforeend", newHtml);
+  }
 
-        let html = '<div class="display-album flex-row"><div class="display-title">%title%</div><div class="display-artist">%artist%</div><div class="display-cover"><img src="%url%"alt=""></div><div class="remove-album">Remove Album X</div>'
-        let newHtml = html.replace('%title%', album.title);
-        newHtml = newHtml.replace('%artist%', album.artist);
-        newHtml = newHtml.replace('%url%', album.url);
-        document.querySelector('.display').insertAdjacentHTML('beforeend', newHtml);
+  clearFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("artist").value = "";
+    document.getElementById("url").value = "";
+  }
 
-
+  deleteAlbum(target) {
+    if (target.className === "remove-album") {
+      target.parentElement.remove();
     }
-
-    clearFields(){
-        document.getElementById('title').value = '';
-        document.getElementById('artist').value = '';
-        document.getElementById('url').value = '';
-    }
-
-    deleteAlbum(target){
-        if(target.className === 'remove-album') {
-            target.parentElement.remove();
-        }
-    }
-}
+  }
+};
 
 //Event Listener for Album
-document.getElementById('form').addEventListener('submit', function(e){
-    //Get Values
-    const title = document.getElementById('title').value;
-    const artist = document.getElementById('artist').value;
-    const url = document.getElementById('url').value;
+document.getElementById("form").addEventListener("submit", function(e) {
+  //Get Values
+  const title = document.getElementById("title").value;
+  const artist = document.getElementById("artist").value;
+  const url = document.getElementById("url").value;
 
-    //inmstantiate new album
-    const album = new Album(title, artist, url);
-    //console.log(album);
+  //instantiate new album
+  const album = new Album(title, artist, url);
+  //console.log(album);
 
-    const ui = new UI();
-    console.log(ui);
+  const ui = new UI();
+  console.log(ui);
 
-    ui.addAlbumToList(album);
+  ui.addAlbumToList(album);
 
-    ui.clearFields();
+  ui.clearFields();
 
-    e.preventDefault();
+  e.preventDefault();
 });
 
 //delete
 
-document.querySelector('.display').addEventListener('click', function(e){
-    const ui = new UI();
-    
-    ui.deleteAlbum(e.target);
+document.querySelector(".display").addEventListener("click", function(e) {
+  const ui = new UI();
 
-    ui.clearFields();
+  ui.deleteAlbum(e.target);
 
-    e.preventDefault();
-})
+  ui.clearFields();
+
+  e.preventDefault();
+});
+
+
+
+
